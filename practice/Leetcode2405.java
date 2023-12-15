06daab389ef5bc740903c5a9ef37d43af66848d8
Swapnil Katkar <skatkar225@gmail.com>
2023-12-15 17:47:21 -0600

    Leetcode 30

---
+++ practice/Leetcode2405.java
@@ -0,0 +1,31 @@
+package practice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Leetcode2405 {
+    public static void main(String[] args) {
+        Leetcode2405 q = new Leetcode2405();
+        int answer = q.partitionString("abacaba");
+        System.out.println("answer = " + answer);
+    }
+
+    public int partitionString(String s) {
+        int count = 0;
+        int right = 0;
+        Map<Character, Integer> map = new HashMap<>();
+
+        for(; right < s.length(); right++) {
+            char ch = s.charAt(right);
+            if(map.containsKey(ch)){
+                count++;
+                map.clear(); // this is needed otherwise there will be an overlap for the same character appearing.
+                // Eg. abacaba
+            }
+            map.put(ch, right);
+        }
+
+        // right went outside the limit but there are still some characters left. Last substring
+        return count + 1;
+    }
+}
